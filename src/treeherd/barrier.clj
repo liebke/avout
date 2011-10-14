45ad4a66e4fa402204e8b5b520495e37295c9aff
David Edgar Liebke <liebke@gmail.com>
2011-10-14 13:55:23 -0400

    Added leave-barrier function

--- src/treeherd/barrier.clj
+++ src/treeherd/barrier.clj
@@ -2,8 +2,40 @@
   (:require [treeherd.client :as tc])
   (:import (java.net InetAddress)))
 
+;; http://wiki.apache.org/hadoop/ZooKeeper/Tutorial
+
 (defn host-name
   ([] (-> (InetAddress/getLocalHost) .getCanonicalHostName)))
+
+(defn leave-barrier
+  "
+  Examples:
+
+    (use '(treeherd client barrier))
+    (def treeherd (client \"127.0.0.1:2181\"))
+
+    ;; start a three process barrier and set leave-on-completion? to false
+    ;; then manually leave as follows:
+
+    (leave-barrier treeherd \"/barrier\")
+
+    (leave-barrier treeherd \"/barrier\" :proc-name \"proc2\")
+
+    (leave-barrier treeherd \"/barrier\" :proc-name \"proc3\")
+
+"
+  ([client barrier-node & {:keys [proc-name] :or {proc-name (host-name)}}]
+     (tc/delete client (str barrier-node "/" proc-name))
+     (future
+       (let [mutex (Object.)
+             watcher (fn [event] (locking mutex (.notify mutex)))]
+         (locking mutex
+           (loop []
+             (if (> (count (tc/children client barrier-node :watcher watcher)) 0)
+               (do
+                 (.wait mutex)
+                 (recur))
+               true)))))))
 
 (defn enter-barrier
   "
@@ -14,25 +46,37 @@
     (defn make-processor [i] (fn [] (println (str \"process \" i \" is running \")) i))
     (delete-all treeherd \"/barrier\")
 
-    (def f0 (enter-barrier treeherd \"/barrier\" (make-processor 0) 3))
+    (def f0 (enter-barrier treeherd \"/barrier\" 3 (make-processor 0)))
 
-    (def f1 (enter-barrier treeherd \"/barrier\" (make-processor 1) 3 :proc-name \"proc2\"))
+    (def f1 (enter-barrier treeherd \"/barrier\" 3 (make-processor 1) :proc-name \"proc2\"))
 
-    (def f2 (enter-barrier treeherd \"/barrier\" (make-processor 2) 3 :proc-name \"proc3\"))
+    (def f2 (enter-barrier treeherd \"/barrier\" 3 (make-processor 2) :proc-name \"proc3\"))
 
-    [@f0 @f1 @f2]
+    @f0
+    @f1
+    @f2
+
+   ;; and the barrier will have no child nodes, since leave-on-completion? defaults to true
+   (children treeherd \"/barrier\")
+
 "
-  ([client barrier-node f N & {:keys [proc-name] :or {proc-name (host-name)}}]
+  ([client barrier-node N f & {:keys [proc-name leave-on-completion?]
+                               :or {proc-name (host-name)
+                                    leave-on-completion? true}}]
+     (if-not (tc/exists client barrier-node)
+       (tc/create client barrier-node :persistent? true))
+     (tc/create client (str barrier-node "/" proc-name))
      (future
        (let [mutex (Object.)
              watcher (fn [event] (locking mutex (.notify mutex)))]
          (locking mutex
-           (if-not (tc/exists client barrier-node)
-             (tc/create client barrier-node :persistent? true))
-           (tc/create client (str barrier-node "/" proc-name))
            (loop []
              (if (>= (count (tc/children client barrier-node :watcher watcher)) N)
-               (f)
+               (let [result (f)]
+                   (when leave-on-completion?
+                     (leave-barrier client barrier-node :proc-name proc-name))
+                   result)
                (do
                  (.wait mutex)
                  (recur)))))))))
+
