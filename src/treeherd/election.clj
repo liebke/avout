c73736c5f33d3ce0452f60ead34c75bcdd93fec5
David Edgar Liebke <liebke@gmail.com>
2011-10-15 01:57:16 -0400

    minor refactoring of election

--- src/treeherd/election.clj
+++ src/treeherd/election.clj
@@ -21,35 +21,11 @@
 ;;       (if node-to-watch (tc/exists client node-to-watch :watcher watcher))
        candidates)))
 
-(defn start-election
+(defn run-election
   "Registers the client in an election, returning their ID.
 
   Examples:
 
-    (use '(treeherd client election))
-    (def treeherd (client \"127.0.0.1:2181\"))
-    (delete-all treeherd \"/election\")
-
-     ;; create a few volunteers
-    (repeatedly 2 #(enter-election treeherd \"/election\"))
-
-    ;; enter electio
-    (def election-results (enter-election treeherd \"/election\"))
-
-    ;; create a few more
-    (repeatedly 5 #(enter-election treeherd \"/election\"))
-
-    (deref (:id election-results))
-    (deref (:leader election-results))
-
-    ;; now remove the current leader
-    (leave-election treeherd (deref (:leader election-results)))
-    ;; and look at the new leader
-    (deref (:leader election-results))
-
-
-
-    ;; ALT
     (use '(treeherd client election))
     (def treeherd (client \"127.0.0.1:2181\"))
     (delete-all treeherd \"/election\")
@@ -59,9 +35,7 @@
 
     (repeatedly 5 #(create-candidate treeherd \"/election\"))
 
-    ;; (repeatedly 5 #(create treeherd \"/election/n-\" :sequential? true))
-
-    (def leader (start-election treeherd \"/election\" candidate))
+    (def leader (run-election treeherd \"/election\" candidate))
     @leader
     (delete treeherd @leader)
     @leader
@@ -72,14 +46,14 @@
   ([client election-node local-candidate]
      (let [mutex (Object.)
            watcher (fn [event] (locking mutex (.notify mutex)))
-           leader (fn [candidates] (-> candidates first first))
+           get-leader (fn [candidates] (-> candidates first first))
            leader-ref (ref nil)]
        (future
          (locking mutex
            (loop [candidates (tc/children client election-node :watcher watcher)]
              (if (seq candidates)
                (do
-                 (dosync (alter leader-ref (fn [_] (leader (sort-candidates election-node local-candidate candidates)))))
+                 (dosync (alter leader-ref (fn [_] (get-leader (sort-candidates election-node local-candidate candidates)))))
                  (.wait mutex)
                  (recur (tc/children client election-node :watcher watcher)))
                (dosync (alter leader-ref (fn [_] nil)))))))
