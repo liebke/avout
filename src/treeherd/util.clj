31fd36901abb0b7a2f22380efc4dffd2d05981ed
David Edgar Liebke <liebke@gmail.com>
2011-10-17 11:07:45 -0400

    added try* macro that pulls the cause from the RuntimeExceptions thrown by Clojure and throws it

---
+++ src/treeherd/util.clj
@@ -0,0 +1,11 @@
+(ns treeherd.util)
+
+(defmacro try*
+  "Unwraps the RuntimeExceptions thrown by Clojure, and rethrows its cause. Only accepts a single expression."
+  ([expression & catches]
+     `(try
+        (try
+          ~expression
+          (catch Throwable e# (throw (.getCause e#))))
+        ~@catches)))
+
