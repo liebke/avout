99152cc5bd1f4ea17566b1cf72d389992001c0a3
David Edgar Liebke <liebke@gmail.com>
2011-10-20 19:21:16 -0400

    addition test case for timeout locking

--- test/treeherd/test/locks_test.clj
+++ test/treeherd/test/locks_test.clj
@@ -47,20 +47,27 @@
            prom1 (promise) ;; use promises to wait for workers to complete before returning result
            prom2 (promise)
            prom3 (promise)
-           worker2 (fn [prom]
+           worker3 (fn [prom]
                      (future (if-lock-with-timeout lock 1 TimeUnit/SECONDS
-                               (dosync (alter state #(conj % [2 1 :FAILED])))
-                               (dosync (alter state #(conj % [2 1 (.getHoldCount lock)]))))
+                               (dosync (alter state #(conj % [3 1 :FAILED])))
+                               (dosync (alter state #(conj % [3 1 (.getHoldCount lock)]))))
+                             (deliver prom @state)))
+           worker2 (fn [prom]
+                     (future (if-lock-with-timeout lock 10 TimeUnit/SECONDS
+                               (dosync (alter state #(conj % [2 1 (.getHoldCount lock)])))
+                               (dosync (alter state #(conj % [2 1 :FAILED]))))
                              (deliver prom @state)))
            worker1 (fn [prom]
                      (future (with-lock lock
                                (worker2 prom2)
+                               (worker3 prom3)
                                (Thread/sleep 5000)
                                (dosync (alter state #(conj % [1 1 (.getHoldCount lock)]))))
                              (deliver prom @state)))]
        (worker1 prom1)
        @prom1
        @prom2
+       @prom3
        @state)))
 
 (deftest locking-test
@@ -78,5 +85,5 @@
 
     (is (= locking-timeout-results (test-lock-with-timeout (ReentrantLock. true))))
 
-    (is (= locking-timeout-results [[2 1 0] [1 1 1]]))
+    (is (= locking-timeout-results [[3 1 0] [1 1 1] [2 1 1]]))
     (.close client)))
