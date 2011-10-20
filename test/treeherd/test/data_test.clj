633da13cbdf7d81ccad605bb29e72b6a240b1a5b
David Edgar Liebke <liebke@gmail.com>
2011-10-20 15:08:36 -0400

    fixed data_test

--- test/treeherd/test/data_test.clj
+++ test/treeherd/test/data_test.clj
@@ -3,10 +3,10 @@
   (:use [clojure.test]))
 
 (deftest to-from-bytes
-  (is "hello" (to-string (to-bytes "hello")))
-  (is 1234 (to-int (to-bytes (Integer/valueOf 1234))))
-  (is 1234 (to-long (to-bytes 1234)))
-  (is 123.456 (to-double (to-bytes 123.456)))
-  (is 123.456 (to-float (to-bytes (float 123.456))))
-  (is 12 (to-short (to-bytes (short 12))))
-  (is \a (to-char (to-bytes \a))))
+  (is (= "hello" (to-string (to-bytes "hello"))))
+  (is (= 1234 (to-int (to-bytes (Integer/valueOf 1234)))))
+  (is (= 1234 (to-long (to-bytes 1234))))
+  (is (= 123.456 (to-double (to-bytes 123.456))))
+  (is (= (float 123.456) (to-float (to-bytes (float 123.456)))))
+  (is (= 12 (to-short (to-bytes (short 12)))))
+  (is (= \a (to-char (to-bytes \a)))))
