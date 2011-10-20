f4cc929637b1184cc34b3bffd68a9c7305885a98
David Edgar Liebke <liebke@gmail.com>
2011-10-19 21:50:39 -0400

    [wip] refactor of locks, addition of tryLock

--- src/treeherd/locks.clj
+++ src/treeherd/locks.clj
@@ -1,134 +1,155 @@
 (ns treeherd.locks
-  (:require [treeherd.client :as tc]
+  (:require [treeherd.client :as zk]
             [treeherd.logger :as log])
-  (:import (java.util.concurrent.locks Lock
-                                       ReentrantLock)))
+  (:import (java.util.concurrent.locks Lock ReentrantLock)))
 
-(defn next-lowest
+(defprotocol DistributedLock
+  (requestNode [this] "Returns the child node representing this lock request instance")
+  (queuedRequests [this] "Returns all queued request-nodes for this distributed lock")
+  (getOwner [this] "Returns the child node that holds the lock.")
+  (deleteLock [this] "Deletes the distributed lock.")
+  (hasLock [this] "Indicates if the current thread has the lock"))
+
+(defprotocol DistributedReentrantLock
+  (getHoldCount [this] "Gets the number of holds for this lock in the current thread"))
+
+(defmacro with-lock
+  ([lock & body]
+     `(try
+        (.lock ~lock)
+        ~@body
+        (finally (.unlock ~lock)))))
+
+(defmacro try-lock
+  ([lock & body]
+     `(try
+        (.tryLock ~lock)
+        ~@body
+        (finally (.unlock ~lock)))))
+
+(defmacro try-lock-with-timeout
+  ([lock timeunits duration & body]
+     `(try
+        (.tryLock ~lock ~timeunits ~duration)
+        ~@body
+        (finally (.unlock ~lock)))))
+
+
+;; internal functions
+
+(defn find-next-lowest-node
   ([node sorted-nodes]
-     (if (= node (first sorted-nodes)) ;; then the node is the lowest
+     (if (= node (first sorted-nodes))
        node
        (loop [[current & remaining] sorted-nodes
               previous nil]
-         (when current ;; if there is no current node, then the node is not in this list
+         (when current
            (if (= node current)
              previous
              (recur remaining current)))))))
 
-(defn make-lock-request-node
-  ([client lock-node req-id]
-     (let [path (str lock-node "/" req-id "-")
-           request-node (tc/create-all client path :sequential? true)]
+(defn create-lock-request-node
+  ([client lock-node request-id]
+     (let [path (str lock-node "/" request-id "-")
+           request-node (zk/create-all client path :sequential? true)]
        (subs request-node (inc (count lock-node))))))
 
-(defn delete-lock-request-node
-  ([client lock-node req-id]
-     (let [path (str lock-node "/" req-id "-")]
-       (tc/delete client path))))
-
-(defn delete-lock*
-  ([client & {:keys [lock-node]
-              :or {lock-node "/lock"}}]
-     (tc/delete-all client lock-node)))
-
-(defn this-request-node*
+(defn find-lock-request-node
   ([client lock-node request-id]
-     (when-let [requests (tc/children client lock-node)]
+     (when-let [requests (zk/children client lock-node)]
        (first (filter #(re-find (re-pattern request-id) %) requests)))))
 
-(defn all-request-nodes*
-  ([client lock-node]
-     (tc/children client lock-node)))
+(defn delete-lock-request
+  ([client lock-node request-id]
+     (zk/delete client (str lock-node "/" (find-lock-request-node client lock-node request-id)))))
 
-(defn lock-holder-node*
+(defn delete-lock
   ([client lock-node]
-     (when-let [requests (tc/children client lock-node)]
-       (first (tc/sort-sequential-nodes requests)))))
+     (zk/delete-all client lock-node)))
 
-(defn request-lock
-  ([client lock-node request-node lock-watcher]
+(defn queued-requests
+  ([client lock-node]
+     (zk/children client lock-node)))
+
+(defn get-owner
+  ([client lock-node]
+     (when-let [requests (zk/children client lock-node)]
+       (first (zk/sort-sequential-nodes requests)))))
+
+(defn submit-lock-request
+  ([client lock-node request-id lock-watcher]
      (let [mutex (Object.)
+           lock-request-node (create-lock-request-node client lock-node request-id)
            watcher (fn [event] (locking mutex (.notify mutex)))]
        (future
          (locking mutex
-           (loop [request-queue (tc/sort-sequential-nodes (tc/children client lock-node))]
-             (if (seq request-queue) ;; if there are no requests in the queue, delete the lock
-               ;; when the node-to-watch is nil, then the requester is no longer in the request queue, so exit
-               (when-let [node-to-watch (next-lowest request-node request-queue)]
-                 (tc/exists client (str lock-node "/" node-to-watch) :watcher watcher)
-                 (when (= request-node node-to-watch) ;; then request-node is the new lock holder
-                   (when lock-watcher (lock-watcher)))
-                 (.wait mutex) ;; wait until zookeeper invokes the watcher, which calls notify
-                 (recur (tc/sort-sequential-nodes (tc/children client lock-node))))
-               (delete-lock* client :lock-node lock-node))))))))
+           (loop [lock-request-queue (zk/sort-sequential-nodes (zk/children client lock-node))]
+             (if (seq lock-request-queue) ;; if no requests in the queue, delete the lock
+               ;; when the node-to-watch is nil, the requester is no longer in the queue, so exit
+               (if-let [node-to-watch (find-next-lowest-node lock-request-node lock-request-queue)]
+                 (if (zk/exists client (str lock-node "/" node-to-watch) :watcher watcher)
+                   (do (when (= lock-request-node node-to-watch)
+                         (lock-watcher)) ;; then lock-request-node is the new lock holder, invoke lock-watcher
+                       (.wait mutex) ;; wait until zookeeper invokes the watcher, which calls notify
+                       (recur (zk/sort-sequential-nodes (zk/children client lock-node))))
+                   ;; if the node-to-watch no longer exists recur to find the next node-to-watch
+                   (recur (zk/sort-sequential-nodes (zk/children client lock-node))))
+                 (delete-lock client lock-node))))))
+       lock-request-node)))
 
-(defn unlock*
-  ([client lock-node request-id]
-     (when-let [req-node (first (this-request-node* client lock-node request-id))]
-       (tc/delete client (str lock-node "/" req-node)))))
-
-(defprotocol DistributedLock
-  (this-request-node [this] "Returns the child node representing this lock request instance")
-  (all-request-nodes [this] "Returns all queued request-nodes for this distributed lock")
-  (lock-holder-node [this] "Returns the child node that holds the lock.")
-  (delete-lock [this] "Deletes the distributed lock.")
-  (getHoldCount [this] "Gets the number of holds for this lock in the current thread")
-  (hasLock [this] "Indicates if the current thread has the lock"))
-
-(defrecord DistributedReentrantLock [client lock-node request-id local-lock]
+(deftype ZKDistributedReentrantLock [client lockNode requestId localLock]
   Lock
   (lock [this]
-    (if (> (.getHoldCount local-lock) 0)
-      (.lock local-lock)
-      (let [request-node (make-lock-request-node client lock-node request-id)
-            condition (.newCondition local-lock)
+    (if (> (.getHoldCount localLock) 0)
+      (.lock localLock)
+      (let [lock-granted (.newCondition localLock)
             watcher (fn []
                       (try
-                        (.lock local-lock)
-                        (.signal condition)
-                        (finally (.unlock local-lock))))]
-        (request-lock client lock-node request-node watcher)
-        (.lock local-lock)
-        (.await condition))))
+                        (.lock localLock)
+                        (.signal lock-granted)
+                        (finally (.unlock localLock))))]
+        (submit-lock-request client lockNode requestId watcher)
+        (.lock localLock)
+        (.await lock-granted))))
 
   (unlock [this]
     (try
-      (if (= (.getHoldCount local-lock) 1)
-        (unlock* client lock-node request-id))
-      (finally (.unlock local-lock))))
+      (if (= (.getHoldCount localLock) 1)
+        (delete-lock-request client lockNode requestId))
+      (finally (.unlock localLock))))
 
   (tryLock [this]
-    (if (> (.getHoldCount local-lock) 0)
-      (.tryLock local-lock)
-      (if-let [locked? (.tryLock local-lock)]
-        (let [request-node (make-lock-request-node client lock-node request-id)]
-          (do (request-lock client lock-node request-node nil)
-              (.sync client lock-node nil nil)
-              (if (.hasLock this)
-                true
-                (do (delete-lock-request-node client lock-node request-id)
-                    false))))
+    (if (> (.getHoldCount localLock) 0)
+      (.tryLock localLock)
+      (if (.tryLock localLock)
+        (do (submit-lock-request client lockNode requestId nil)
+            (.sync client lockNode nil nil)
+            (if (.hasLock this)
+              true
+              (do (delete-lock-request-node client lockNode requestId)
+                  false)))
         false)))
 
   DistributedLock
-  (this-request-node [this]
-    (this-request-node* client lock-node request-id))
+  (requestNode [this]
+    (find-lock-request-node client lockNode requestId))
 
-  (all-request-nodes [this]
-    (all-request-nodes* client lock-node))
+  (queuedRequests [this]
+    (queued-requests client lockNode))
 
-  (lock-holder-node [this]
-    (lock-holder-node* client lock-node))
+  (getOwner [this]
+    (get-owner client lockNode))
 
-  (delete-lock [this]
-    (delete-lock* client lock-node))
-
-  (getHoldCount [this]
-    (.getHoldCount local-lock))
+  (deleteLock [this]
+    (delete-lock client lockNode))
 
   (hasLock [this]
-    (and (= (.this-request-node this) (.lock-holder-node this))
-         (.isHeldByCurrentThread local-lock))))
+    (and (.isHeldByCurrentThread localLock)
+         (= (.requestNode this) (.getOwner this))))
+
+  DistributedReentrantLock
+  (getHoldCount [this]
+    (.getHoldCount localLock)))
 
 (defn distributed-lock
   "
@@ -138,12 +159,12 @@
     (use '(treeherd client locks))
     (def treeherd (client \"127.0.0.1\"))
 
-    (delete-lock* treeherd)
+    (delete-lock treeherd \"/lock\")
 
     (def dlock (distributed-lock treeherd))
     (future (.lock dlock) (println \"first lock acquired\") (flush) (Thread/sleep 5000))
-    (.lock-holder-node dlock)
-    (delete treeherd (str \"/lock/\" (.lock-holder-node dlock)))
+    (.getOwner dlock)
+    (delete treeherd (str \"/lock/\" (.getOwner dlock)))
 
     (def dlock2 (distributed-lock treeherd))
     (future
@@ -161,19 +182,19 @@
       (println \"unlocked second lock: \" (.getHoldCount dlock2))
       (.unlock dlock2)
       (println \"unlocked second lock again: \" (.getHoldCount dlock2)))
-    (.lock-holder-node dlock2)
-    (delete treeherd (str \"/lock/\" (.lock-holder-node dlock2)))
+    (.getOwner dlock2)
+    (delete treeherd (str \"/lock/\" (.getOwner dlock2)))
 
 
     (future (.lock dlock) (println \"first lock acquired acquired again\") (flush))
-    (.lock-holder-node dlock)
+    (.getOwner dlock)
     (.unlock dlock)
 
-    (.all-request-nodes dlock)
+    (.queuedRequests dlock)
 
    ;; from another repl
    ;; connect to dlock lock
-   (def dlock-reconnected (distributed-lock treeherd :request-id (.lock-holder-node dlock)))
+   (def dlock-reconnected (distributed-lock treeherd :request-id (.getOwner dlock)))
    (.unlock dlock-reconnected)
 
    (.delete dlock)
@@ -182,5 +203,5 @@
   ([client & {:keys [lock-node request-id]
               :or {lock-node "/lock"
                    request-id (.toString (java.util.UUID/randomUUID))}}]
-     (DistributedReentrantLock. client lock-node request-id (ReentrantLock. true))))
+     (ZKDistributedReentrantLock. client lock-node request-id (ReentrantLock. true))))
 
