f671a8f2708db52ea7a43d53f9fd35459a31eb83
David Edgar Liebke <liebke@gmail.com>
2011-10-26 20:57:37 -0400

    refactored mazurka.barrier

--- src/mazurka/barrier.clj
+++ src/mazurka/barrier.clj
@@ -1,5 +1,6 @@
 (ns mazurka.barrier
-  (:require [zookeeper :as zk])
+  (:require [zookeeper :as zk]
+            [zookeeper.util :as zutil])
   (:import (java.net InetAddress)))
 
 ;; http://zookeeper.apache.org/doc/r3.3.3/api/index.html
@@ -10,78 +11,72 @@
 
 
 (defn host-name
-  ([] (-> (InetAddress/getLocalHost) .getCanonicalHostName)))
+  ([] (.getCanonicalHostName (InetAddress/getLocalHost))))
 
-(defn leave-barrier
-  "
-  Examples:
-
-    (use 'zookeeper)
-    (use 'mazurka.barrier)
-    (def client (client \"127.0.0.1:2181\"))
-
-    ;; start a three process barrier and set leave-on-completion? to false
-    ;; then manually leave as follows:
-
-    (leave-barrier client \"/barrier\")
-
-    (leave-barrier client \"/barrier\" :proc-name \"proc2\")
-
-    (leave-barrier client \"/barrier\" :proc-name \"proc3\")
-
-"
-  ([client barrier-node & {:keys [proc-name] :or {proc-name (host-name)}}]
-     (zk/delete client (str barrier-node "/" proc-name))
-     (future
-       (let [mutex (Object.)
-             watcher (fn [event] (locking mutex (.notify mutex)))]
-         (locking mutex
-           (loop []
-             (if (> (count (zk/children client barrier-node :watcher watcher)) 0)
-               (do
-                 (.wait mutex)
-                 (recur))
-               true)))))))
+(defn exit-barrier
+  ([client & {:keys [barrier-node proc-name]
+              :or {barrier-node "/barrier"
+                   proc-name (host-name)}}]
+     (let [mutex (Object.)
+           watcher (fn [event] (locking mutex (.notify mutex)))]
+       (zk/delete client (str barrier-node "/ready"))
+       (locking mutex
+         (loop []
+           (when-let [children (seq (sort (or (zk/children client barrier-node) nil)))]
+             (cond
+               ;; the last node deletes itself and the barrier node, letting all the processes exit
+               (= (count children) 1)
+                 (zk/delete-all client barrier-node)
+               ;; first node watches the second, waiting for it to be deleted
+               (= proc-name (first children))
+                 (do (when (zk/exists client
+                                      (str barrier-node "/" (second children))
+                                      :watcher watcher)
+                       (.wait mutex))
+                     (recur))
+               ;; rest of the nodes delete their own node, and then watch the
+               ;; first node, waiting for it to be deleted
+               :else
+                 (do (zk/delete client (str barrier-node "/" proc-name))
+                     (when (zk/exists client
+                                      (str barrier-node "/" (first children))
+                                      :watcher watcher)
+                       (.wait mutex))
+                     (recur)))))))))
 
 (defn enter-barrier
-  "
+"
   Examples:
 
     (use 'zookeeper)
     (use 'mazurka.barrier)
     (def client (client \"127.0.0.1:2181\"))
-    (defn make-processor [i] (fn [] (println (str \"process \" i \" is running \")) i))
-    (delete-all client \"/barrier\")
 
-    (def f0 (enter-barrier client \"/barrier\" 3 (make-processor 0)))
+    (enter-barrier client 2 #(println \"First process is running\"))
 
-    (def f1 (enter-barrier client \"/barrier\" 3 (make-processor 1) :proc-name \"proc2\"))
+    ;; From another REPL, execute the following
 
-    (def f2 (enter-barrier client \"/barrier\" 3 (make-processor 2) :proc-name \"proc3\"))
+    (use 'zookeeper)
+    (use 'mazurka.barrier)
+    (def client (client \"127.0.0.1:2181\"))
 
-    @f0
-    @f1
-    @f2
-
-   ;; and the barrier will have no child nodes, since leave-on-completion? defaults to true
-   (children client \"/barrier\")
+    (enter-barrier client 2 #(println \"Second process is running\") :proc-name \"node2\")
 
 "
-  ([client barrier-node N f & {:keys [proc-name leave-on-completion?]
-                               :or {proc-name (host-name)
-                                    leave-on-completion? true}}]
-     (zk/create-all client (str barrier-node "/" proc-name))
-     (future
-       (let [mutex (Object.)
-             watcher (fn [event] (locking mutex (.notify mutex)))]
-         (locking mutex
-           (loop []
-             (if (>= (count (zk/children client barrier-node :watcher watcher)) N)
-               (let [result (f)]
-                   (when leave-on-completion?
-                     (leave-barrier client barrier-node :proc-name proc-name))
-                   result)
-               (do
-                 (.wait mutex)
-                 (recur)))))))))
-
+  ([client n f & {:keys [barrier-node proc-name double-barrier?]
+                  :or {barrier-node "/barrier"
+                       proc-name (host-name)
+                       double-barrier? true}}]
+     (let [mutex (Object.)
+           watcher (fn [event] (locking mutex (.notify mutex)))]
+       (locking mutex
+         (zk/create-all client (str barrier-node "/" proc-name))
+         (if (>= (count (zk/children client barrier-node)) n)
+           (zk/create client (str barrier-node "/ready") :async? true)
+           (do (zk/exists client (str barrier-node "/ready") :watcher watcher :async? true)
+               (.wait mutex)))
+         (let [results (f)]
+           (if double-barrier?
+             (exit-barrier client :barrier-node barrier-node :proc-name proc-name)
+             (zk/delete-all client barrier-node))
+           results)))))
---
+++ test/mazurka/test/barrier_test.clj
@@ -0,0 +1,24 @@
+(ns mazurka.test.barrier-test
+  (:use [mazurka barrier]
+        [zookeeper]
+        [clojure test])
+  (:import (java.util UUID)))
+
+(deftest barrier-test
+  (let [client (connect "127.0.0.1:2181")
+        result (ref [])
+        make-processor (fn [sleep]
+                         (fn []
+                           (Thread/sleep (* 100 sleep))
+                           (dosync (alter result #(conj % :done)))))
+        barrier-node (str "/test-barrier-" (UUID/randomUUID))]
+    (doseq [i (range 5)]
+      (future
+        (try
+          (enter-barrier client 4 (make-processor i)
+                         :barrier-node barrier-node
+                         :proc-name (str "proc-" i))
+          (catch Throwable e (.printStackTrace e)))))
+    (is (= (count @result) 0))
+    (enter-barrier client 4 (make-processor 0) :barrier-node barrier-node)
+    (is (= (count @result) 6))))
