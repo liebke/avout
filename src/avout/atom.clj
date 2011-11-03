019f6a74a30648832ea7483a91334a28c4465fbe
David Edgar Liebke <liebke@gmail.com>
2011-11-03 12:21:32 -0400

    Added avout.atom

---
+++ src/avout/atom.clj
@@ -0,0 +1,75 @@
+(ns avout.atom
+  (:require [zookeeper :as zk]
+            [zookeeper.data :as data])
+  (:import (org.apache.zookeeper KeeperException$BadVersionException)
+           (clojure.lang IDeref)))
+
+;; PROTOCOLS
+
+(defprotocol AtomData
+  (nodeName [this] "Returns the ZooKeeper node name associated with this AtomData.")
+  (getVersionedValue [this] "Returns a map containing the :value and a :version
+    which may just be the current value or a version number and is used in
+    compareAndSet to determine if an update should occur.")
+  (compareAndSetValue [this new-value expected-version])
+  (resetValue [this new-value]))
+
+(defprotocol AtomReference
+  (swap [this f] [this f args])
+  (reset [this new-value]))
+
+
+;; ZK Atom
+
+(defn serialize-form
+  "Serializes a Clojure form to a byte-array."
+  ([form]
+     (data/to-bytes (pr-str form))))
+
+(defn deserialize-form
+  "Deserializes a byte-array to a Clojure form."
+  ([form]
+     (read-string (data/to-string form))))
+
+(deftype ZKAtomData [client name]
+  AtomData
+  (nodeName [this] name)
+  (getVersionedValue [this]
+    (let [{:keys [data stat]} (zk/data client name)]
+      {:value (deserialize-form data), :version (:version stat)}))
+  (compareAndSetValue [this new-value current-version]
+    (try
+      (zk/set-data client name (serialize-form new-value) current-version)
+      (catch KeeperException$BadVersionException e
+        (compareAndSetValue this new-value current-version))))
+  (resetValue [this new-value] (zk/set-data client name (serialize-form new-value) -1)))
+
+(deftype ZKAtomReference [client atomData]
+  IDeref
+  (deref [this] (:value (.getVersionedValue atomData)))
+  AtomReference
+  (swap [this f] (.swap this f nil))
+  (swap [this f args]
+    (let [{:keys [value version]} (.getVersionedValue atomData)
+          new-value (apply f value args)]
+      (when (.compareAndSetValue atomData new-value version)
+        new-value)))
+  (reset [this new-value]
+    (when (.resetValue atomData new-value)
+      new-value)))
+
+(defn zk-atom
+  ([client name]
+     (zk-atom client name nil))
+  ([client name init-value]
+     (zk/create client name)
+     (doto (ZKAtomReference. client (ZKAtomData. client name))
+       (.reset init-value))))
+
+(defn !swap
+  "Cannot use standard swap! because Clojure expects a clojure.lang.Atom."
+  ([atom f & args] (.swap atom f args)))
+
+(defn !reset
+  "Cannot use standard reset! because Clojure expects a clojure.lang.Atom."
+  ([atom new-value] (.reset atom new-value)))
