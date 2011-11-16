97df321a9da6a1da8c8f2d1692ca48c16db08b08
David Edgar Liebke <liebke@gmail.com>
2011-11-15 20:04:34 -0500

    more detailed output for write-skew-test

--- test/avout/test/write_skew_test.clj
+++ test/avout/test/write_skew_test.clj
@@ -12,12 +12,10 @@
           c (zk-ref client "/c" 0)
           d (zk-ref client "/d" [])]
       (dotimes [i n]
-        (print ".")
         (future
           (try
             (deliver (nth times i) (timer #(dosync!! client (alter!! d conj (alter!! c inc)))))
             (catch Throwable e (println "write-skew-test deliver ex: ") (.printStackTrace e)))))
-      (println "submitted")
       [times c d]))
 
   (defn proces-results [[time-proms c d] n]
@@ -34,25 +32,31 @@
        :c @c
        :d @d
        :pass? (= @c (count @d))
+       :skew-count (- @c (count @d))
+       :n n
        :avg-time-interval avg-time-interval
        :avg-time-interval-per-thread avg-time-interval-per-thread}))
 
   (defn analyze-write-skew-test [client n]
     (try
       (let [res (proces-results (write-skew-test client n) n)]
-       (assoc (if (and (= (:c res) n) (:pass? res))
-                res
-                (assoc res :pass? false))
-         :n n))
+        (if (and (= (:c res) n) (:pass? res))
+          res
+          (assoc res :pass? false)))
       (catch Throwable e (println "analyze-write-skew-test: " e (.printStackTrace e)))))
 
 (deftest write-skew
   (let [client (connect "127.0.0.1")
         test-results (atom [])
         _ (dotimes [i 50]
-            (print i ": ")
-            (swap! test-results conj (analyze-write-skew-test client 25)))
+            (let [threads (inc (rand-int 25))
+                  res (analyze-write-skew-test client threads)]
+              (println (str i " (" threads " threads): write-skews = " (:skew-count res) ", pass? " (:pass? res)))
+              (swap! test-results conj res)))
         avg-time-interval-per-thread (/ (apply + (map :avg-time-interval-per-thread @test-results)) (count @test-results))]
     (println "run results: " (map :pass? @test-results))
     (println "avg-time-interval-per-thread: " avg-time-interval-per-thread)
+    (println "total write skews: " (reduce + (map :skew-count @test-results)))
+    (println "probability of write skew: " (/ (reduce + (map :skew-count @test-results))
+                                              (reduce + (map :n @test-results))))
     (is (reduce #(and %1 %2) (map :pass? @test-results)))))
