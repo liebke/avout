0fd1ec2a23425e8388066a6ae51e45eb3294097e
David Edgar Liebke <liebke@gmail.com>
2011-11-21 13:12:59 -0500

    clean up

--- plugins/avout-mongo/project.clj
+++
@@ -1,5 +0,0 @@
-(defproject avout-mongo "0.5.0-SNAPSHOT"
-  :description "An example of an Avout-based, MongoDB-backed Distributed Atom"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [avout "0.5.0-SNAPSHOT"]
-                 [congomongo "0.1.7"]])
--- project.clj
+++
@@ -1,4 +0,0 @@
-(defproject avout "0.5.0-SNAPSHOT"
-  :description "Avout extends Clojure's syntax and semantics for managing in-memory state to distributed state."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [zookeeper-clj "0.9.0"]])
