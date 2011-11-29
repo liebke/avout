47c6744a8332cff188eab019de16d0ffd5bfb8fc
David Edgar Liebke <liebke@gmail.com>
2011-11-29 11:32:26 -0500

    updated avout-mongo version number

---
+++ plugins/avout-mongo/README.markdown
@@ -0,0 +1,11 @@
+# avout-mongo
+
+An example of a MongoDB-based, Avout distributed Atom and Ref.
+
+<h3>License</h3>
+
+Avout is distributed under the Eclipse Public License, the same as Clojure.
+
+<h3>Copyright</h3>
+
+Avout is Copyright © 2011 David Liebke and Relevance, Inc
--- plugins/avout-mongo/project.clj
+++ plugins/avout-mongo/project.clj
@@ -1,5 +1,6 @@
-(defproject avout-mongo "0.5.0-SNAPSHOT"
-  :description "An example of an Avout-based, MongoDB-backed Distributed Atom"
+(defproject avout-mongo "0.5.0"
+  :description "An example of an Avout-based, MongoDB-backed Distributed Atom and Ref"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [avout "0.5.0-SNAPSHOT"]
-                 [congomongo "0.1.7"]])
+                 [avout "0.5.0"]
+                 [congomongo "0.1.7"]]
+  :dev-dependencies [[lein-clojars "0.7.0"]])
