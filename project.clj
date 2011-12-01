dd8c6d81dbba2127201e7c47f37ff1823743b943
David Edgar Liebke <liebke@gmail.com>
2011-12-01 11:35:27 -0500

    incremented version number

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject avout "0.5.0"
+(defproject avout "0.5.1"
   :description "Avout: Distributed State in Clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [zookeeper-clj "0.9.0"]]
+                 [zookeeper-clj "0.9.1"]]
   :dev-dependencies [[lein-clojars "0.7.0"]])
