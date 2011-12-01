1dc04f0b139e702a9de736a4392b014674cd806e
David Edgar Liebke <liebke@gmail.com>
2011-12-01 11:42:02 -0500

    updated version number of avout-mongo

--- plugins/avout-mongo/project.clj
+++ plugins/avout-mongo/project.clj
@@ -1,6 +1,6 @@
-(defproject avout-mongo "0.5.0"
+(defproject avout-mongo "0.5.1"
   :description "An example of an Avout-based, MongoDB-backed Distributed Atom and Ref"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [avout "0.5.0"]
+                 [avout "0.5.1"]
                  [congomongo "0.1.7"]]
   :dev-dependencies [[lein-clojars "0.7.0"]])
