228ab530df27c48a5842c9fee0172b0709e291db
David Edgar Liebke <liebke@gmail.com>
2013-04-09 09:52:29 -0400

    Update dependencies and version number.

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject avout "0.5.3"
+(defproject avout "0.5.4"
   :description "Avout: Distributed State in Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [zookeeper-clj "0.9.1"]]
-  :dev-dependencies [[lein-clojars "0.7.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [zookeeper-clj "0.9.3"]])
