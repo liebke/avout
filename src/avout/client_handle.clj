f52d27059c5750e0c58f0660b67e0567e5c24e49
Alex Chin <achin@42six.com>
2014-03-14 11:31:40 -0400

    Create new protocol and factory function for holding ZooKeeper connections.

---
+++ src/avout/client_handle.clj
@@ -0,0 +1,15 @@
+(ns avout.client-handle
+  (:require [zookeeper :as zk]))
+
+(defprotocol ClientHandle
+  (getClient [this]))
+
+(defn make-zookeeper-client-handle
+  "Creates a new ZooKeeper client handle that simply wraps a single ZooKeeper
+  connection. Calls to getClient will always return the same ZooKeeper
+  instance, even if it has been disconnected."
+  [& args]
+  (let [client (apply zk/connect args)]
+    (reify ClientHandle
+      (getClient [this]
+        client))))
